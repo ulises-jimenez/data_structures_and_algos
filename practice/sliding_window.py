8022df22da42efd9019738a6a3ce456cefe6737c
ulisesjimenez <ulises.andreas.jimenez@gmail.com>
2022-06-29 11:38:36 +0200

    adding new files

--- practice/sliding_window.py
+++ practice/sliding_window.py
@@ -1,72 +1,55 @@
 from typing import List
 
-example_array = [2, 3, 4, 1, 5]
+subarray_example = [2, 3, 4, 1, 5]
 k = 2
 
 
-def calculate_average_of_sub_arrays(array: List,
-                                    sub_array_len: int):
-    array_of_averages = []
-    beginning_of_last_window = len(array) - sub_array_len + 1
-    current_array_sum = 0
-    for pointer in range(beginning_of_last_window):
-        pointer_window_end = pointer + k
-        print(pointer, pointer_window_end)
-        print(array[pointer:pointer_window_end])
-        if pointer == 0:
-            for sub_window_pointer in range(pointer, pointer_window_end):
-                current_array_sum = current_array_sum + array[sub_window_pointer]
-        else:
-            current_array_sum = current_array_sum - array[pointer] + array[pointer_window_end]
-        current_array_avg = current_array_sum / 5
-        array_of_averages.append(current_array_avg)
-    print(array_of_averages)
+def find_max_subarray_sum_brute(subarray: List[int], subarray_len: int):
+    current_max_sum = 0
+    current_max_entries = []
+    start_pointer = 0
+    end_pointer = subarray_len - 1
+    beginning_pointer_last_subarray = len(subarray) - subarray_len + 1
+    print(subarray)
+    for entry in range(start_pointer, beginning_pointer_last_subarray):
+        current_sum = 0
+        current_end_pointer = entry + subarray_len
+        current_entries = subarray[entry:current_end_pointer]
+        print(f'current entries: {current_entries}')
+        for sum_component in current_entries:
+            current_sum += sum_component
+        if current_sum > current_max_sum:
+            current_max_sum = current_sum
+            current_max_entries = current_entries
+
+    print(current_max_sum, current_max_entries)
 
 
-def find_maximum_subarray_sum_on2(array: List,
-                                  sub_array_len: int):
-    beginning_of_last_window = len(array) - sub_array_len
-    subarrays = []
-    current_max_subarray_sum = 0
-    max_subarray = []
-    for pointer in range(beginning_of_last_window + 1):
-        current_subarray_sum = 0
-        current_subarray = []
-        for sub_array_pointer in range(pointer, pointer + sub_array_len):
-            current_subarray_sum += array[sub_array_pointer]
-            current_subarray.append(array[sub_array_pointer])
-        if current_subarray_sum > current_max_subarray_sum:
-            current_max_subarray_sum = current_subarray_sum
-            max_subarray = current_subarray
-        subarrays.append(current_subarray_sum)
-    print(f'subarray with max sum: {max_subarray}')
-    print(f'max subarray sum: {current_max_subarray_sum}')
-
-
-def find_maximum_subarray_sum_on(array: List,
-                                 sub_array_len: int):
-    max_sum = 0
-    max_sub_array = []
+def find_max_subarray_optimized(subarray: List[int], subarray_len: int):
+    starting_pointer = 0
+    current_max_sum = 0
+    current_max_sequence = []
+    last_subarray_pointer_start = len(subarray) - subarray_len + 1
+    print(subarray)
     current_sum = 0
-    current_sub_array = []
-    window_start_pointer = 0
-    for window_end_pointer in range(len(array)):
-        if window_end_pointer < sub_array_len:
-            current_sum += array[window_end_pointer]
-            current_sub_array.append(array[window_end_pointer])
+    for pointer in range(starting_pointer, last_subarray_pointer_start):
+        current_entries = subarray[pointer:pointer + subarray_len]
+        print(f'current entries {current_entries}')
+        if pointer > 0:
+            current_sum -= subarray[pointer - 1]
+            current_sum += subarray[pointer + 1]
         else:
-            current_sum = current_sum + array[window_end_pointer] - array[window_start_pointer]
-            current_sub_array.append(array[window_end_pointer])
-            current_sub_array.pop(0)
-            if current_sum > max_sum:
-                max_sum = current_sum
-            else:
-                pass
-            window_start_pointer += 1
-    print(f'max subarray sum: {max_sum}')
+            for sum_entry in current_entries:
+                current_sum += sum_entry
+        if current_sum > current_max_sum:
+            current_max_sum = current_sum
+            current_max_sequence = current_entries
+    print(current_max_sum, current_max_sequence)
+
+
+def main():
+    print('main')
 
 
 if __name__ == '__main__':
-    # calculate_average_of_sub_arrays(array=example_array, sub_array_len=k)
-    find_maximum_subarray_sum_on2(array=example_array, sub_array_len=k)
-    find_maximum_subarray_sum_on(array=example_array, sub_array_len=k)
+    main()
