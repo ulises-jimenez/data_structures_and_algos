8b98108b07d862322e0efe0716783bce288d7ef8
ulisesjimenez <ulises.andreas.jimenez@gmail.com>
2022-11-29 23:14:59 -0800

    finished sliding window with variable window size

--- practice/sliding_window.py
+++ practice/sliding_window.py
@@ -1,82 +1,72 @@
-import math
 from typing import List
 
-subarray_example = [2, 1, 5, 2, 3, 2]
-s = 7
+subarray_example = [2, 1, 5, 1, 3, 2]
+k = 3
 
 
-def find_max_subarray_sum_brute(subarray: List[int], subarray_len: int):
-    current_max_sum = 0
-    current_max_entries = []
-    start_pointer = 0
-    end_pointer = subarray_len - 1
-    beginning_pointer_last_subarray = len(subarray) - subarray_len + 1
-    print(subarray)
-    for entry in range(start_pointer, beginning_pointer_last_subarray):
-        current_sum = 0
-        current_end_pointer = entry + subarray_len
-        current_entries = subarray[entry:current_end_pointer]
-        print(f'current entries: {current_entries}')
-        for sum_component in current_entries:
-            current_sum += sum_component
-        if current_sum > current_max_sum:
-            current_max_sum = current_sum
-            current_max_entries = current_entries
-
-    print(current_max_sum, current_max_entries)
+def find_average_of_subarrays(number_list: List[int], length: int):
+    avg_num_list = []
+    number_list_len = len(number_list)
+    end_num_for_range = number_list_len - length + 1
+    for num in range(end_num_for_range):
+        relevent_numbers = number_list[num:num + length]
+        number_mean = sum(relevent_numbers) / length
+        avg_num_list.append(number_mean)
+    print(avg_num_list)
 
 
-def find_max_subarray_optimized(subarray: List[int], subarray_len: int):
-    starting_pointer = 0
-    current_max_sum = 0
-    current_max_sequence = []
-    last_subarray_pointer_start = len(subarray) - subarray_len + 1
-    print(subarray)
-    current_sum = 0
-    for pointer in range(starting_pointer, last_subarray_pointer_start):
-        current_entries = subarray[pointer:pointer + subarray_len]
-        print(f'current entries {current_entries}')
-        if pointer > 0:
-            current_sum -= subarray[pointer - 1]
-            current_sum += subarray[pointer + 1]
+def find_average_of_subarrays_optimized(number_list: List[int], length: int):
+    avg_num_list = []
+    num_list_len = len(number_list)
+    end_of_range = num_list_len - length + 1
+    for num in range(end_of_range):
+        if num == 0:
+            relevent_numbers = number_list[num:num + length]
         else:
-            for sum_entry in current_entries:
-                current_sum += sum_entry
-        if current_sum > current_max_sum:
-            current_max_sum = current_sum
-            current_max_sequence = current_entries
-    print(current_max_sum, current_max_sequence)
+            relevent_numbers.pop(0)
+            relevent_numbers.append(number_list[num + length - 1])
+        number_mean = sum(relevent_numbers) / length
+        avg_num_list.append(number_mean)
+    print(avg_num_list)
 
 
-def find_shortest_subarray_larger_than_l(input_array: List[int], min_sum: int):
-    print(input_array)
-    correct_sum = math.inf
-    correct_subarray = []
-    starting_pointer = 0
-    current_sum = 0
-    for end_pointer in range(len(input_array)):
-        current_subarray = input_array[starting_pointer: end_pointer + 1]
-        current_sum += input_array[end_pointer]
-        while current_sum >= min_sum:
-            if correct_sum == math.inf:
-                correct_sum = current_sum
-                correct_subarray = current_subarray
-            elif len(current_subarray) < len(correct_subarray):
-                correct_sum = current_sum
-                correct_subarray = current_subarray
-            else:
-                pass
-            starting_pointer += 1
-            current_subarray = input_array[starting_pointer: end_pointer + 1]
-            current_sum -= input_array[starting_pointer - 1]
-    if correct_sum == math.inf:
-        print([], 0)
-    else:
-        print(correct_subarray, correct_sum)
+def find_largest_sum_array_brute(number_list, length):
+    largest_subarray = []
+    for num in range(len(number_list) - length + 1):
+        current_subarray = number_list[num:num + length]
+        current_sum = sum(current_subarray)
+        if num == 0:
+            largest_subarray_sum = current_sum
+            largest_subarray = current_subarray
+        else:
+            if current_sum > largest_subarray_sum:
+                largest_subarray_sum = current_sum
+                largest_subarray = current_subarray
+    print(largest_subarray)
+    print(largest_subarray_sum)
+
+
+def find_largest_sum_array_optimized(number_list, length):
+    largest_subarray = []
+    largest_subarray_sum = 0
+    window_sum = 0
+    for num in range(len(number_list) - length + 1):
+        if num == 0:
+            for subarray_num in range(num, num + length):
+                window_sum += number_list[subarray_num]
+        else:
+            window_sum -= number_list[num - 1]
+            window_sum += number_list[num + length - 1]
+        if window_sum > largest_subarray_sum:
+            largest_subarray_sum = window_sum
+            largest_subarray = number_list[num:num + length]
+    print(largest_subarray)
+    print(largest_subarray_sum)
 
 
 def main():
-    find_shortest_subarray_larger_than_l(input_array=subarray_example, min_sum=s)
+    find_largest_sum_array_brute(number_list=subarray_example, length=k)
+    find_largest_sum_array_optimized(number_list=subarray_example, length=k)
 
 
 if __name__ == '__main__':
