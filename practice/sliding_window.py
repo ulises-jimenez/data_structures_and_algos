ef43b99f7732e699f38017ef09362e0db7d070f0
ulisesjimenez <ulises.andreas.jimenez@gmail.com>
2022-12-12 15:15:53 -0800

    finished finding largest substring in string with k distinct letters

--- practice/sliding_window.py
+++ practice/sliding_window.py
@@ -98,20 +98,65 @@ def find_smallest_subarray_with_required_sum(min_required_sum, number_list):
     return min_length
 
 
-def main():
-    print("Smallest subarray length: "
-          + str(find_smallest_subarray_with_required_sum(7, [2, 1, 5, 2, 3, 2])))
-    print("Smallest subarray length: "
-          + str(find_smallest_subarray_with_required_sum(7, [2, 1, 5, 2, 8])))
-    print("Smallest subarray length: "
-          + str(find_smallest_subarray_with_required_sum(8, [3, 4, 1, 1, 6])))
+def check_for_longest_substring_with_k_distinct(string_array, k):
+    window_start = 0
+    max_len = 0
+    char_hash_count = {}
+    for window_end in range(len(string_array)):
+        current_letter = string_array[window_end]
+        try:
+            char_hash_count[current_letter] = char_hash_count[current_letter] + 1
+        except KeyError:
+            char_hash_count[current_letter] = 1
+        while len(char_hash_count) > k:
+            window_start_letter = string_array[window_start]
+            window_start += 1
+            char_hash_count[window_start_letter] = char_hash_count[window_start_letter] - 1
+            if char_hash_count[window_start_letter] == 0:
+                del (char_hash_count[window_start_letter])
+        max_len = max(max_len, window_end - window_start + 1)
+    return max_len
 
-    print("Smallest subarray length: "
-          + str(smallest_subarray_sum(7, [2, 1, 5, 2, 3, 2])))
-    print("Smallest subarray length: "
-          + str(smallest_subarray_sum(7, [2, 1, 5, 2, 8])))
-    print("Smallest subarray length: "
-          + str(smallest_subarray_sum(8, [3, 4, 1, 1, 6])))
+
+def longest_substring_with_k_distinct(str1, k):
+    window_start = 0
+    max_length = 0
+    char_frequency = {}
+
+    # in the following loop we'll try to extend the range [window_start, window_end]
+    for window_end in range(len(str1)):
+        right_char = str1[window_end]
+        if right_char not in char_frequency:
+            char_frequency[right_char] = 0
+        char_frequency[right_char] += 1
+
+        # shrink the sliding window, until we are left with 'k' distinct characters in
+        # the char_frequency
+        while len(char_frequency) > k:
+            left_char = str1[window_start]
+            char_frequency[left_char] -= 1
+            if char_frequency[left_char] == 0:
+                del char_frequency[left_char]
+            window_start += 1  # shrink the window
+        # remember the maximum length so far
+        max_length = max(max_length, window_end - window_start + 1)
+    return max_length
+
+
+def main():
+    print("Length of the longest substring: "
+          + str(check_for_longest_substring_with_k_distinct("araaci", 2)))
+    print("Length of the longest substring: "
+          + str(check_for_longest_substring_with_k_distinct("araaci", 1)))
+    print("Length of the longest substring: "
+          + str(check_for_longest_substring_with_k_distinct("cbbebi", 3)))
+
+    print("Length of the longest substring: "
+          + str(longest_substring_with_k_distinct("araaci", 2)))
+    print("Length of the longest substring: "
+          + str(longest_substring_with_k_distinct("araaci", 1)))
+    print("Length of the longest substring: "
+          + str(longest_substring_with_k_distinct("cbbebi", 3)))
 
 
 if __name__ == '__main__':
